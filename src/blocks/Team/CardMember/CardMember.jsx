f9d8d1c64edc8ee4343be84fc519f6b329613827
Шевчук Богдан Олегович <bohdan.shevchuk@kyivcity.gov.ua>
2024-03-17 17:30:35 +0200

    changed photo name

--- src/blocks/Team/CardMember/CardMember.jsx
+++ src/blocks/Team/CardMember/CardMember.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from './cardMember.module.scss';
 
 function CardMember({ name, role, imageSrc }) {
+  const path = 'https://github.com/miraculous-bog/dozrili/blob/main/src/content_images/photos/team_bohdan_turiv.png';
   return (
     <div className={styles.card}>
       <img src={imageSrc} alt={`Фото ${name}`} className={styles.photo} />
--- src/blocks/Team/Team.jsx
+++ src/blocks/Team/Team.jsx
@@ -8,7 +8,7 @@ import data_team from '../../helper/data_team';
 import styles from './team.module.scss';
 
 const Team = () => {
-	const path = '../../../content_images/photos/';
+	const path = 'https://github.com/miraculous-bog/dozrili/blob/main/src/content_images/photos/';
 	console.log(data_team);
 	return (
 		<div className={styles.team}>
@@ -19,7 +19,7 @@ const Team = () => {
 				{data_team.map((cardMemberData) => 
 					(<CardMember name={cardMemberData.name}
 						role={cardMemberData.role}
-						imageSrc={`${path}${cardMemberData.imageSrc}`}
+						imageSrc={`https://github.com/miraculous-bog/dozrili/blob/main/src/content_images/photos/${cardMemberData.imageSrc}?raw=true`}
 						key={cardMemberData.name} />)
 				)}
 
