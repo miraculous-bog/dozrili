96dc8838ea88c20a9e37a471c9c8a83c2a37819e
Шевчук Богдан Олегович <bohdan.shevchuk@kyivcity.gov.ua>
2024-04-10 01:54:43 +0300

    change img

--- client/src/components/JoinBtn/JoinBtn.jsx
+++ client/src/components/JoinBtn/JoinBtn.jsx
@@ -4,7 +4,7 @@ import styles from './joinBtn.module.scss';
 
 const JoinBtn = ({ isWideMode }) => {
   return (
-    <Link to="https://translate.google.com/">
+    <Link to="https://t.me/dozrili_bot?start=w19479776">
       <div className={styles.button}>
         <p>приєднатися{isWideMode ? null : (<br />)} до спільноти</p>
       </div >
--- client/src/components/SupportBtn/SupportBtn.jsx
+++ client/src/components/SupportBtn/SupportBtn.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import styles from './supportBtn.module.scss';
 
 const SupportBtn = ({ isWideMode }) => {
   return (
+    <Link to='https://send.monobank.ua/jar/7C743Le9ff'>
     <div className={styles.button}>
       <p>&#8372; підтримати {isWideMode ? 'спільноту' : null}</p>
     </div>
+    </Link>
   );
 };
 
