ce2e86d5b7ce61b961052bec091f112e7eb5c88c
Шевчук Богдан Олегович <bohdan.shevchuk@kyivcity.gov.ua>
2024-06-07 17:14:26 +0300

    hidden folder test

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const path = require('path');
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const mongoose = require('mongoose');
 const cors = require("cors");
